9fcd9e041f1f6ab1cdac4a5369a2adf4cc7a0cb9
Kirill Makoveev <119239992+aredaw0@users.noreply.github.com>
2025-03-31 22:18:04 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/shortener_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise Exception("///////!")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
